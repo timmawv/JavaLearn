68fdbcbf0189206505480e2beb5b42733c2c42b7
timcol1 <rrggo76@gmail.com>
2022-05-29 16:39:41 +0300

    finish task1

--- src/avlyakulov/timur/task1/Main.java
+++ src/avlyakulov/timur/task1/Main.java
@@ -1,4 +1,12 @@
 package avlyakulov.timur.task1;
 
 public class Main {
+    public static void main(String[] args) {
+        FloorSumNeighboring sum = new FloorSumNeighboring();
+        int[] arr = sum.sumNeighboring();
+        for (int i : arr)
+            System.out.println("Elements:" + i);
+        System.out.println();
+    }
+
 }
