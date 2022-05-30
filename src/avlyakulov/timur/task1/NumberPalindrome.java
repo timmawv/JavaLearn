b499626a27a2df03d64810c28851381f523ae8dd
timcol1 <rrggo76@gmail.com>
2022-05-30 14:07:12 +0300

    finish task with NumberPalindrome

--- src/avlyakulov/timur/task1/NumberPalindrome.java
+++ src/avlyakulov/timur/task1/NumberPalindrome.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 
 //Числа-палиндромы, значения которых в прямом и обратном порядке совпадают.
 public class NumberPalindrome {
-    public void findPalindromes() {
+    public static String reverse(String src) {
+        String result = "";
+        for (int i = 0; i < src.length(); i++) {
+            result = src.charAt(i) + result;
+        }
+        return result;
+    }
+    public static void findPalindromes() {
         Scanner in = new Scanner(System.in);
         int len = in.nextInt();
         int[] mas = new int[len];
@@ -12,7 +19,9 @@ public class NumberPalindrome {
             mas[i] = in.nextInt();
         }
         for (Integer i : mas) {
-            String temp = i.toString();
+            String temp = reverse(i.toString());
+            if (i.toString().equals(temp))
+                System.out.println(i);
         }
     }
 }
