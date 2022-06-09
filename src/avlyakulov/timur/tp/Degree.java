5864c640e8a781dc5a4f31ec27f18276ae6165ca
timcol1 <rrggo76@gmail.com>
2022-06-09 21:41:39 +0300

    1 lab TPR

--- src/avlyakulov/timur/tp/Degree.java
+++ src/avlyakulov/timur/tp/Degree.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Degree {
     public static double toDegree() {
+        System.out.println("Y=(x^n)%m");
         double x = 0;
         x = fillNumber(x);
         double n = 0;
@@ -11,20 +12,20 @@ public class Degree {
         double m = 0;
         m = fillNumber(m);
         double result;
-        x = fillNumber(x);
-        n = fillNumber(n);
-        m = fillNumber(m);
-        return result = Math.pow(x, n) % m;
+        return result = (Math.pow(x, n)) % m;
+    }
+    public static int pow(int value){
+        return value*value;
     }
 
     public static double fillNumber(double x) {
-        System.out.println("Enter your number");
+        System.out.println("Enter your number ");
         Scanner in = new Scanner(System.in);
         x = in.nextDouble();
         return x;
     }
 
     public static void printNumber(double x) {
-        System.out.println("Your number" + x);
+        System.out.println("Your number " + x);
     }
 }
--- src/avlyakulov/timur/tp/Main.java
+++ src/avlyakulov/timur/tp/Main.java
@@ -2,9 +2,13 @@ package avlyakulov.timur.tp;
 
 public class Main {
     public static void main(String[] args) {
+        long start = Runtime.getRuntime().freeMemory();
         long m = System.currentTimeMillis();
         double result = Degree.toDegree();
-        System.out.println((double) (System.currentTimeMillis() - m));
         Degree.printNumber(result);
+        System.out.println((double) (System.currentTimeMillis() - m));
+        long end = Runtime.getRuntime().freeMemory();
+        long memoTaken = start - end;
+        System.out.println(memoTaken);
     }
 }
