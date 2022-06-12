cbde22b5c996f6cee03724689acfd4eae34ab23d
timcol1 <avlyakulovtimur@gmail.com>
2022-06-12 13:52:10 +0300

    finish task with Degree

--- src/avlyakulov/timur/tp/Degree.java
+++ src/avlyakulov/timur/tp/Degree.java
@@ -1,31 +1,19 @@
 package avlyakulov.timur.tp;
 
-import java.util.Scanner;
-
 public class Degree {
-    public static double toDegree() {
-        System.out.println("Y=(x^n)%m");
-        double x = 0;
-        x = fillNumber(x);
-        double n = 0;
-        n = fillNumber(n);
-        double m = 0;
-        m = fillNumber(m);
-        double result;
-        return result = (Math.pow(x, n)) % m;
+    public static double toDegree(double x, double n, double m) {
+        return (pow(x, n)) % m;
     }
-    public static int pow(int value){
-        return value*value;
+    public static double pow(double value, double powValue) {
+        double result = 1;
+        for (int i = 1; i <= powValue; i++) {
+            result = result * value;
+        }
+        return result;
     }
 
-    public static double fillNumber(double x) {
-        System.out.println("Enter your number ");
-        Scanner in = new Scanner(System.in);
-        x = in.nextDouble();
-        return x;
-    }
 
     public static void printNumber(double x) {
-        System.out.println("Your number " + x);
+        System.out.print("Your number is " + x);
     }
 }
--- src/avlyakulov/timur/tp/Main.java
+++ src/avlyakulov/timur/tp/Main.java
@@ -4,11 +4,14 @@ public class Main {
     public static void main(String[] args) {
         long start = Runtime.getRuntime().freeMemory();
         long m = System.currentTimeMillis();
-        double result = Degree.toDegree();
+        double result = Degree.toDegree(8,4,4086);
         Degree.printNumber(result);
+        System.out.println();
+        System.out.print("Часу затрачено ");
         System.out.println((double) (System.currentTimeMillis() - m));
         long end = Runtime.getRuntime().freeMemory();
         long memoTaken = start - end;
+        System.out.print("Памяті затрачено ");
         System.out.println(memoTaken);
     }
 }
