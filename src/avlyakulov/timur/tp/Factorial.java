05157e0f5e38753eb0c2cff69c8882d6b19b3d60
timcol1 <avlyakulovtimur@gmail.com>
2022-06-13 11:30:38 +0300

    finish task with Factorial

--- src/avlyakulov/timur/tp/Factorial.java
+++ src/avlyakulov/timur/tp/Factorial.java
@@ -1,5 +1,35 @@
 package avlyakulov.timur.tp;
 
-public class Factorial {
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
+public class Factorial {
+    public static int getFactorialMultiply(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+    public static int getFactorialRecursion(int f) {
+        if (f <= 1) {
+            return 1;
+        }
+        else {
+            return f * getFactorialRecursion(f - 1);
+        }
+    }
+    public static int getFactorialStream(int f) {
+        if (f <= 1) {
+            return 1;
+        }
+        else {
+            return IntStream.rangeClosed(2, f).reduce((x, y) -> x * y).getAsInt();
+        }
+    }
+    public static int fillNumber(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your number ");
+        return in.nextInt();
+    }
 }
--- src/avlyakulov/timur/tp/Main.java
+++ src/avlyakulov/timur/tp/Main.java
@@ -2,16 +2,11 @@ package avlyakulov.timur.tp;
 
 public class Main {
     public static void main(String[] args) {
-        long start = Runtime.getRuntime().freeMemory();
+        int f = Factorial.fillNumber();
         long m = System.currentTimeMillis();
-        double result = Degree.toDegree(8,4,4086);
-        Degree.printNumber(result);
+        System.out.println("Result: " + Factorial.getFactorialStream(f));
         System.out.println();
         System.out.print("Часу затрачено ");
         System.out.println((double) (System.currentTimeMillis() - m));
-        long end = Runtime.getRuntime().freeMemory();
-        long memoTaken = start - end;
-        System.out.print("Памяті затрачено ");
-        System.out.println(memoTaken);
     }
 }
