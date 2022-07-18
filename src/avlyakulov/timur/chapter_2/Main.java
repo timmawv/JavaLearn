69086a5b7cbed59b51387f22f2ae49295ec48eb4
timcol1 <avlyakulovtimur@gmail.com>
2022-07-18 14:28:58 +0300

    finish task with NumberSystemTranslation

--- src/avlyakulov/timur/chapter_2/Main.java
+++ src/avlyakulov/timur/chapter_2/Main.java
@@ -2,6 +2,6 @@ package avlyakulov.timur.chapter_2;
 
 public class Main {
     public static void main(String[] args) {
-        CheckBelongsToInterval.checkNumber();
+        NumberSystemTranslation.translationNumber();
     }
 }
