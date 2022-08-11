5368276401f5cacee40059b25a9847afb36f81e5
timcol1 <avlyakulovtimur@gmail.com>
2022-08-11 12:21:22 +0300

    create UserService

---
+++ src/avlyakulov/timur/book/chapter_3/user/service/UserService.java
@@ -0,0 +1,23 @@
+package avlyakulov.timur.book.chapter_3.user.service;
+
+import avlyakulov.timur.book.chapter_3.user.dao.UserDao;
+import avlyakulov.timur.book.chapter_3.user.entity.User;
+
+public class UserService {
+    private final UserDao userDao = new UserDao();
+        public void create (User user){
+            userDao.create(user);
+        }
+        public void delete (String id) {
+            userDao.delete(id);
+        }
+        public void update (User user){
+            userDao.update(user);
+        }
+        public User findById (String id) {
+            return userDao.findById(id);
+        }
+        public User[] findAll () {
+            return userDao.findAll();
+        }
+}
