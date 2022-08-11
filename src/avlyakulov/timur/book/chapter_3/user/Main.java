8688be74012855539d59f9cea50a75a30355c6c5
timcol1 <avlyakulovtimur@gmail.com>
2022-08-11 13:05:08 +0300

    create UserController

--- src/avlyakulov/timur/book/chapter_3/user/Main.java
+++ src/avlyakulov/timur/book/chapter_3/user/Main.java
@@ -1,6 +1,9 @@
 package avlyakulov.timur.book.chapter_3.user;
 
+import avlyakulov.timur.book.chapter_3.user.controller.UserController;
+
 public class Main {
     public static void main(String[] args) {
+        new UserController().run();
     }
 }
