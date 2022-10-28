40422abb716ed28515f9034920717fa178abd502
timcol1 <avlyakulovtimur@gmail.com>
2022-10-28 11:27:01 +0300

    add task with Notepad

---
+++ src/avlyakulov/timur/epam/chapter_5/controller/NotepadController.java
@@ -0,0 +1,4 @@
+package avlyakulov.timur.epam.chapter_5.controller;
+
+public class NotepadController {
+}
--- src/avlyakulov/timur/practise/Practise.java
+++ src/avlyakulov/timur/practise/Practise.java
@@ -1,17 +1,15 @@
 package avlyakulov.timur.practise;
 
 public class Practise {
-    class PractiseInner {
-        private int num = 0;
+
+    public void doSomething () {
+
     }
 
 
 
+
     public static void main(String[] args) {
-        Practise practise = new Practise();
-        Practise.PractiseInner practiseInner = practise.new PractiseInner();
-        ++practiseInner.num;
-        System.out.println(practiseInner.num);
 
     }
 }
