73de06c7a46950eb41b4bc79f183d1cc7472aa8c
timcol1 <avlyakulovtimur@gmail.com>
2022-11-04 10:30:58 +0200

    add task with Mobile and add method to add model

---
+++ src/avlyakulov/timur/epam/chapter_5/mobile/entity/Company.java
@@ -0,0 +1,5 @@
+package avlyakulov.timur.epam.chapter_5.mobile.entity;
+
+public enum Company {
+    XIAOMI,IPHONE,SAMSUNG,ASUS,PIXEL
+}
---
+++ src/avlyakulov/timur/epam/chapter_5/mobile/entity/Mobile.java
@@ -0,0 +1,45 @@
+package avlyakulov.timur.epam.chapter_5.mobile.entity;
+/*
+Создать класс Mobile с внутренним классом, с помощью объектов которого
+можно хранить информацию о моделях телефонов и их свойствах.
+ */
+public class Mobile {
+    private Model[] models;
+    private int current;
+
+    public Mobile () {
+        models = new Model[4];
+    }
+
+    public void addModelToMobile (Model model) {
+        if (current == models.length) {
+            Model[] models1 = new Model[models.length + 1];
+            System.arraycopy(models,0,models1,0,models.length);
+            models = models1;
+        }
+        models[current] = model;
+        ++current;
+    }
+
+
+    public class Model {
+        private Company name;
+        private int price;
+        private int capacityOfBattery;
+
+        public Model (Company name,int price,int capacityOfBattery) {
+            this.name = name;
+            this.price = price;
+            this.capacityOfBattery = capacityOfBattery;
+        }
+
+        @Override
+        public String toString() {
+            return "Model{" +
+                    "name=" + name +
+                    ", price=" + price +
+                    ", capacityOfBattery=" + capacityOfBattery +
+                    '}' + '\n';
+        }
+    }
+}
