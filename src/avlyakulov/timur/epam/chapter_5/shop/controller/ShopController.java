3c6b4022081dca01d1736c6a7064dadff6245023
timcol1 <avlyakulovtimur@gmail.com>
2022-11-14 09:41:06 +0200

    add task with Shop

---
+++ src/avlyakulov/timur/epam/chapter_5/shop/controller/ShopController.java
@@ -0,0 +1,4 @@
+package avlyakulov.timur.epam.chapter_5.shop.controller;
+
+public class ShopController {
+}
---
+++ src/avlyakulov/timur/epam/chapter_5/shop/entity/Shop.java
@@ -0,0 +1,7 @@
+package avlyakulov.timur.epam.chapter_5.shop.entity;
+/*
+Создать класс Shop с внутренним классом, с помощью объектов которого
+можно хранить информацию об отделах, товарах и услугах.
+ */
+public class Shop {
+}
