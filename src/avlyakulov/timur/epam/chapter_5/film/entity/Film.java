aed951306d665f75cb9ae7c18c78d0e031969b5c
timcol1 <avlyakulovtimur@gmail.com>
2022-11-15 10:19:13 +0200

    add task with film

---
+++ src/avlyakulov/timur/epam/chapter_5/film/entity/Film.java
@@ -0,0 +1,12 @@
+package avlyakulov.timur.epam.chapter_5.film.entity;
+/*
+Создать класс Фильм с внутренним классом, с помощью объектов которо-
+го можно хранить информацию о продолжительности, жанре и режиссерах
+фильма.
+ */
+public class Film {
+
+    public class Genre {
+
+    }
+}
