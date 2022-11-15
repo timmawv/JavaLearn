414e02b817772d7fbf7b1c4110d273773cde3299
timcol1 <avlyakulovtimur@gmail.com>
2022-11-15 10:39:14 +0200

    finish with entity

--- src/avlyakulov/timur/epam/chapter_5/film/entity/Film.java
+++ src/avlyakulov/timur/epam/chapter_5/film/entity/Film.java
@@ -1,12 +1,59 @@
 package avlyakulov.timur.epam.chapter_5.film.entity;
+
+import java.util.Arrays;
+
 /*
-Создать класс Фильм с внутренним классом, с помощью объектов которо-
-го можно хранить информацию о продолжительности, жанре и режиссерах
-фильма.
+Создать класс Фильм с внутренним классом, с помощью объектов которого
+можно хранить информацию о продолжительности, жанре и режиссерах фильма.
  */
 public class Film {
 
-    public class Genre {
+    private Genre[] films;
+    private int current;
 
+    public Film () {
+        films = new Genre[2];
+    }
+
+    public void addFilm (Genre film) {
+        if (current == films.length) {
+            Genre[] films1 = new Genre[films.length + 1];
+            System.arraycopy(films,0,films1,0,films.length);
+            films = films1;
+        }
+        films[current] = film;
+        ++current;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "films =" + Arrays.toString(films) +
+                '}';
+    }
+
+    public class Genre {
+        private String duration;
+        private String producer;
+        private String nameOfGenre;
+        private String nameOfFilm;
+
+        public Genre (String duration,String producer,String nameOfGenre,String nameOfFilm) {
+            this.duration = duration;
+            this.producer = producer;
+            this.nameOfGenre = nameOfGenre;
+            this.nameOfFilm = nameOfFilm;
+        }
+
+
+        @Override
+        public String toString() {
+            return "Genre of film{" +
+                    "duration='" + duration + '\'' +
+                    ", producer='" + producer + '\'' +
+                    ", nameOfGenre='" + nameOfGenre + '\'' +
+                    ", nameOfFilm='" + nameOfFilm + '\'' +
+                    '}';
+        }
     }
 }
