e0deb25ecd33e7f50d3ac2321e800f2eea25ce96
timcol1 <avlyakulovtimur@gmail.com>
2022-11-22 10:59:14 +0200

    look out at record

--- src/avlyakulov/timur/practise/Main.java
+++ src/avlyakulov/timur/practise/Main.java
@@ -1,12 +1,10 @@
 package avlyakulov.timur.practise;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main{
 
     public static void main(String[] args) {
-
+        RecordImutable recordImutable = new RecordImutable("Tim",12);
+        System.out.println(recordImutable.name());
+        System.out.println(recordImutable);
     }
 }
--- src/avlyakulov/timur/practise/Practise.java
+++ src/avlyakulov/timur/practise/Practise.java
@@ -1,15 +1,7 @@
 package avlyakulov.timur.practise;
 
 public class Practise {
-    public int size = 0;
 
-    static class Inner {
-        public void incrementSize() {
-            new Practise().size +=1;
-        }
 
-        public static void main(String[] args) {
 
-        }
-    }
 }
