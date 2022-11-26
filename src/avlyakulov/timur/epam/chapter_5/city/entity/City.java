f6eeab643b5e825d38d76b0fd69ffc6b14a98726
timcol1 <avlyakulovtimur@gmail.com>
2022-11-26 20:13:08 +0200

    finish with entity

--- src/avlyakulov/timur/epam/chapter_5/city/entity/City.java
+++ src/avlyakulov/timur/epam/chapter_5/city/entity/City.java
@@ -1,14 +1,65 @@
 package avlyakulov.timur.epam.chapter_5.city.entity;
+
+import java.util.Arrays;
+
 /*
 Создать класс City с внутренним классом, с помощью объектов которого
 можно хранить информацию о проспектах, улицах, площадях.
  */
 public class City {
+    private final String name;
     private CityDistrict[] cityDistricts;
+    private final int population;
+    private final boolean isCapital;
 
-    public class CityDistrict {
-        private String name;
-        private TypeOfDistrict typeOfDistrict;
+    private int current;
 
+    public City (String name, int population) {
+        this.name = name;
+        cityDistricts = new CityDistrict[4];
+        this.population = population;
+        isCapital = this.name.toLowerCase().equals("kiev");
+    }
+
+    public void addToCityDistrict (CityDistrict cityDistrict) {
+        if (current == cityDistricts.length) {
+            CityDistrict[] cityDistrict1 = new CityDistrict[cityDistricts.length + 1];
+            System.arraycopy(cityDistricts,0,cityDistrict1,0,cityDistricts.length);
+            cityDistricts = cityDistrict1;
+        }
+        cityDistricts[current] = cityDistrict;
+        ++current;
+    }
+
+    public int getPopulation () {
+        return population;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", cityDistricts=" + Arrays.toString(cityDistricts) +
+                ", population=" + population +
+                ", isCapital=" + isCapital +
+                '}';
+    }
+
+    public  class CityDistrict {
+        private final TypeOfDistrict typeOfDistrict;
+        private final String name;
+
+        public CityDistrict ( String typeOfDistrict, String name) {
+            this.typeOfDistrict = TypeOfDistrict.valueOf(typeOfDistrict.toUpperCase());
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "CityDistrict{" +
+                    "typeOfDistrict=" + typeOfDistrict.getTranslation() +
+                    ", name='" + name + '\'' +
+                    '}' + '\n';
+        }
     }
 }
