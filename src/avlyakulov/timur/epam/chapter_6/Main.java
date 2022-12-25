b99892390c1243560f6d3d7df0d7dbdc5e86b500
timcol1 <avlyakulovtimur@gmail.com>
2022-12-25 11:09:01 +0200

    clear warning

--- src/avlyakulov/timur/epam/chapter_6/Main.java
+++ src/avlyakulov/timur/epam/chapter_6/Main.java
@@ -2,7 +2,6 @@ package avlyakulov.timur.epam.chapter_6;
 
 
 import avlyakulov.timur.epam.chapter_6.city.CityController;
-import avlyakulov.timur.epam.chapter_6.train.CarriageController;
 
 public class Main {
     public static void main(String[] args) {
