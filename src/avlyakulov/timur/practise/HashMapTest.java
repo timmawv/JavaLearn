5edc085b878a8c5691519d9da7b89ccd8c33dc60
timcol1 <avlyakulovtimur@gmail.com>
2023-01-09 16:15:46 +0200

    review HashMap

---
+++ src/avlyakulov/timur/practise/HashMapTest.java
@@ -0,0 +1,22 @@
+package avlyakulov.timur.practise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"Timur");
+        map.put(2,"Alex");
+        map.put(3,"Dima");
+        map.putIfAbsent(4,"Oleg");
+        System.out.println(map.get(2));
+        map.remove(2);
+        System.out.println(map.containsValue("Timur"));
+        System.out.println(map.containsKey(1));
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+        System.out.println(map.entrySet());
+        System.out.println(map);
+    }
+}
