2c397b20a9b43401fa5aa467fd517c754292b2f9
timcol1 <avlyakulovtimur@gmail.com>
2023-01-10 17:19:04 +0200

    optimize taks with anagrams

--- src/avlyakulov/timur/epam/chapter_7/anagram/AnagramPractise.java
+++ src/avlyakulov/timur/epam/chapter_7/anagram/AnagramPractise.java
@@ -13,32 +13,68 @@ import java.util.stream.Collectors;
 Определить, являются ли слова анаграммами
 т.е. можно ли из одного слова составить другое перестановкой букв.
  */
+
+
 public class AnagramPractise {
-    public static void main(String[] args) {
+    public WordAnagramUtil fillWordAndAnagram() {
+        String word = "";
+        String anagram = "";
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("Enter the word ");
-            String word = reader.readLine();
+            word = reader.readLine();
             System.out.println("Enter the word that you think it is anagram");
-            String anagram = reader.readLine();
-            BiPredicate<String, String> predicateTestLength = (w1, w2) -> w1.length() == w2.length();
-            Function<String,String[]> splitWordToArray = s-> s.split(("(?!^)"));
-            System.out.println(Arrays.stream(splitWordToArray.apply(word)).sorted().collect(Collectors.toList()));
-            BiPredicate<List, List> checkEqualList = (l1,l2) -> l1.equals(l2);
-
-
-        if (!predicateTestLength.test(word, anagram))
-            System.out.println("Your words are not anagrams");
-        else {
-            if (checkEqualList.test(Arrays.stream(splitWordToArray.apply(word)).sorted().collect(Collectors.toList()),Arrays.stream(splitWordToArray.apply(anagram)).sorted().collect(Collectors.toList())))
-                System.out.println("Your words are anagrams");
-            else System.out.println("Your words are not anagrams");
+            anagram = reader.readLine();
+        } catch (IOException | IllegalArgumentException e) {
+            System.out.println("You have enter the wrong value " + e.getMessage());
         }
+        return new WordAnagramUtil(word, anagram);
+    }
+    public boolean checkWordsLength (WordAnagramUtil wordAnagramUtil) {
+        BiPredicate<String, String> predicateTestLength = (w1, w2) -> w1.length() == w2.length();
+        return predicateTestLength.test(wordAnagramUtil.getWord(), wordAnagramUtil.getAnagram());
+    }
+    public List<String> converToList (String str) {
+        Function<String, String[]> splitWordToArray = s -> s.split(("(?!^)"));
+        return Arrays.stream(splitWordToArray.apply(str)).sorted().collect(Collectors.toList());
+    }
+    public boolean checkLengthLists (List<String> listWord,List<String> listAnagram) {
+        BiPredicate<List<String>, List<String>> checkEqualList = List::equals;
+        return checkEqualList.test(listWord,listAnagram);
+    }
+    public void run() {
+        WordAnagramUtil wordAnagramUtil = fillWordAndAnagram();
+        if (!checkWordsLength(wordAnagramUtil)) {
+            System.out.println("Your words are not anagrams");
+        }
+        else {
+            List<String> listWord = converToList(wordAnagramUtil.getWord());
+            List<String> listAnagram = converToList(wordAnagramUtil.getAnagram());
+            if (checkLengthLists(listWord,listAnagram))
+                System.out.println("Your words are anagrams");
+            else System.out.println("No your words are not anagrams");
+        }
+    }
 
-    } catch(IOException |
-    IllegalArgumentException e)
 
-    {
-        System.out.println("You have enter the wrong value " + e.getMessage());
+    public static void main(String[] args) {
+        new AnagramPractise().run();
     }
 }
+
+class WordAnagramUtil {
+    String word;
+    String anagram;
+
+    public WordAnagramUtil(String word, String anagram) {
+        this.word = word;
+        this.anagram = anagram;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getAnagram() {
+        return anagram;
+    }
 }
