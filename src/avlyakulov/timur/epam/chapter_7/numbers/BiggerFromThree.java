2208a3b380da87134de29bea2ee0e2644aea05d4
timcol1 <avlyakulovtimur@gmail.com>
2023-01-12 17:28:07 +0200

    finish task with BiggerFromThree

---
+++ src/avlyakulov/timur/epam/chapter_7/numbers/BiggerFromThree.java
@@ -0,0 +1,31 @@
+package avlyakulov.timur.epam.chapter_7.numbers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.function.Function;
+
+/*
+Дано три разных целых числа. Реализовать лямбда-выражение, которое на-
+ходит наибольшее из этих трех чисел.
+ */
+public class BiggerFromThree {
+    public static void main(String[] args) {
+        System.out.println("Enter 3 different numbers");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int firstNum = Integer.parseInt(reader.readLine());
+            int secondNum = Integer.parseInt(reader.readLine());
+            int thirdNum = Integer.parseInt(reader.readLine());
+            CheckMore checkMore = (a, b, c) -> {
+                if (a > b && a > c)
+                    return a;
+                else if (b > a && b > c) {
+                    return b;
+                } else return c;
+            };
+            System.out.println(checkMore.checkMoreFromThree(firstNum, secondNum, thirdNum));
+        } catch (IOException | IllegalArgumentException e) {
+            System.out.println("You have enter the wrong value " + e.getMessage());
+        }
+    }
+}
---
+++ src/avlyakulov/timur/epam/chapter_7/numbers/CheckMore.java
@@ -0,0 +1,5 @@
+package avlyakulov.timur.epam.chapter_7.numbers;
+@FunctionalInterface
+public interface CheckMore {
+    int checkMoreFromThree (int a,int b,int c);
+}
