24df2894950a4c9929ca9c5f3ccd977bcc9fb06c
timcol1 <avlyakulovtimur@gmail.com>
2023-01-16 12:09:25 +0200

    finish task with number of Fibonacci

--- src/avlyakulov/timur/epam/chapter_7/numbers/fibonacci/CheckNumForFibonacci.java
+++ src/avlyakulov/timur/epam/chapter_7/numbers/fibonacci/CheckNumForFibonacci.java
@@ -3,21 +3,24 @@ package avlyakulov.timur.epam.chapter_7.numbers.fibonacci;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.function.Predicate;
+import java.util.Arrays;
+import java.util.function.IntPredicate;
+
 
 /*
-Определить, является ли число элементом Фибоначчи с помощью лямбда-
-выражений.
+Определить, является ли число элементом Фибоначчи с помощью лямбда выражений.
  */
 public class CheckNumForFibonacci {
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Enter the number that you want to check from numbers Fibonacci");
             int num = Integer.parseInt(reader.readLine());
-            Predicate<Integer> checkFibonacci;
-
-
-        }
-        catch (IOException | IllegalArgumentException e) {
+            int[] numbersFibonacci = {1, 1, 2, 3, 5, 8, 13, 21, 34};
+            IntPredicate checkNumFibonacci = a -> a == num;
+            if (Arrays.stream(numbersFibonacci).anyMatch(checkNumFibonacci))
+                System.out.println("Yes your number is number of Fibonacci");
+            else System.out.println("No your number is not number of Fibonacci");
+        } catch (IOException | IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
---
+++ src/avlyakulov/timur/practise/NumbersFibonacci.java
@@ -0,0 +1,18 @@
+package avlyakulov.timur.practise;
+
+//реализицая чисел фибоначчи
+public class NumbersFibonacci {
+    public static void main(String[] args) {
+        System.out.println("Numbers of Fibonacci ");
+        int n0 = 1;
+        int n1 = 1;
+        int n2;
+        System.out.print(n0 + " " + n1 + " ");
+        for (int i = 3; i < 10; i++) {
+            n2 = n0 + n1;
+            System.out.print(n2 + " ");
+            n0 = n1;
+            n1 = n2;
+        }
+    }
+}
