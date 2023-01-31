8ab81e5fe5208f5963096a3e2f6dd4887e339678
timcol1 <avlyakulovtimur@gmail.com>
2023-01-31 13:44:49 +0200

    equals SBuilder and SBuffer

--- src/avlyakulov/timur/epam/chapter_8/example/strings/StringTest2.java
+++ src/avlyakulov/timur/epam/chapter_8/example/strings/StringTest2.java
@@ -4,7 +4,18 @@ public class StringTest2 {
     public static void main(String[] args) {
         // method substring() - задаем индексы начального и конечного эелемента и обрезаем строку
         String test = "Impressive";
-        System.out.println(test.substring(0,6));
+        String test1 = new String("Impressive");
+        System.out.println(test == test1);
+        test1 = test1.intern();
+        System.out.println(test == test1);
+        System.out.println("Look out StringBuilder and StringBuffer equals");
+        StringBuilder builder = new StringBuilder("Java");
+        StringBuffer buffer = new StringBuffer("Java");
+        System.out.println("Equals with equals()");
+        System.out.println(builder.equals(buffer));
+        System.out.println(buffer.equals(builder));
+        System.out.println("Equals with toString().equals()");
+        System.out.println(builder.toString().equals(buffer.toString()));
 
     }
 }
