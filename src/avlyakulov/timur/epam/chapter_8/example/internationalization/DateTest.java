72e5ee2f190b3e5848d02f66956a20781fa1f6e2
timcol1 <avlyakulovtimur@gmail.com>
2023-02-02 11:24:25 +0200

    review Locale and NumberFormat

---
+++ src/avlyakulov/timur/epam/chapter_8/example/internationalization/DateTest.java
@@ -0,0 +1,7 @@
+package avlyakulov.timur.epam.chapter_8.example.internationalization;
+
+public class DateTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/avlyakulov/timur/epam/chapter_8/example/internationalization/LangTest.java
@@ -0,0 +1,28 @@
+package avlyakulov.timur.epam.chapter_8.example.internationalization;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LangTest {
+    public static void main(String[] args) {
+        ResourceBundle bundleUa = ResourceBundle.getBundle("text", new Locale("uk", "UA"));
+        System.out.println(bundleUa.getString("str1"));
+        System.out.println(bundleUa.getString("str2"));
+        System.out.println("Number Format");
+        Locale localeUa = new Locale("uk", "UA");
+        double number = 21234.567;
+        System.out.println(number);
+        NumberFormat format = NumberFormat.getInstance(localeUa);
+        System.out.println("Наше число в текущей UA Locale " + format.format(number));
+        System.out.println("Наше число в дефолтной локале");
+        format = NumberFormat.getInstance();
+        System.out.println("Наше число в текущей default Locale " + format.format(number));
+        Locale localeFr = Locale.FRANCE;
+        format = NumberFormat.getInstance(localeFr);
+        System.out.println("Наше число в текущей FR Locale " + format.format(number));
+        Locale localeGr = Locale.GERMAN;
+        format = NumberFormat.getInstance(localeGr);
+        System.out.println("Наше число в текущей Germany Locale " + format.format(number));
+    }
+}
