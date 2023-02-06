3930b177cc7ec696a2674f60ca36f15c3d541e02
timcol1 <avlyakulovtimur@gmail.com>
2023-02-06 12:49:30 +0200

    finish task with count word in text

---
+++ src/avlyakulov/timur/epam/chapter_8/example/variant_a/task_10/FindCharacterFrequency.java
@@ -0,0 +1,10 @@
+package avlyakulov.timur.epam.chapter_8.example.variant_a.task_10;
+/*
+В тексте найти и напечатать n символов (и их количество), встречающихся
+наиболее часто.
+ */
+public class FindCharacterFrequency {
+    public static void main(String[] args) {
+
+    }
+}
