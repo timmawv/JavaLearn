9a76c625be0b0e2be06de716f15c213681472385
timcol1 <avlyakulovtimur@gmail.com>
2023-02-06 13:47:08 +0200

    finish task with FindMoreLetters

---
+++ src/avlyakulov/timur/epam/chapter_8/example/variant_a/task_11/FindMoreLetters.java
@@ -0,0 +1,27 @@
+package avlyakulov.timur.epam.chapter_8.example.variant_a.task_11;
+
+import java.util.Arrays;
+
+/*
+Найти, каких букв, гласных или согласных, больше в каждом предложении
+текста.
+ */
+public class FindMoreLetters {
+    public static void main(String[] args) {
+        String text = "Our text is very interesting to read.So what do you want to know.It is amazing!How many people are here?";
+        String[] sentences = text.split("[.?!]");
+        System.out.println(Arrays.toString(sentences));
+        int counterConsonants = 0;
+        for (String sen : sentences) {
+            char[] senChar = sen.replace(" ", "").toLowerCase().toCharArray();
+            for (char ch : senChar)
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    ++counterConsonants;
+                }
+            if (counterConsonants > senChar.length - counterConsonants)
+                System.out.println("Гласных больше и их количество: " + counterConsonants);
+            else System.out.println("Согласных больше и их количество: " + (senChar.length - counterConsonants));
+            counterConsonants = 0;
+        }
+    }
+}
