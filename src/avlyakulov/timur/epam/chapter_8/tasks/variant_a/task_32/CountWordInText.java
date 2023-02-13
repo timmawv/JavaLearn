06707e2e1c7d76e5af3657ec6b5121b1f13bd9c9
timcol1 <avlyakulovtimur@gmail.com>
2023-02-13 12:11:29 +0200

    finish task with CountWordInText

---
+++ src/avlyakulov/timur/epam/chapter_8/tasks/variant_a/task_32/CountWordInText.java
@@ -0,0 +1,29 @@
+package avlyakulov.timur.epam.chapter_8.tasks.variant_a.task_32;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+Подсчитать, сколько раз заданное слово входит в текст.
+ */
+public class CountWordInText {
+    public static void main(String[] args) {
+        String text = "Наше слово Было его важное слово! Которое есть, Важным слово в нашем тексте. Поэтому Мы его пишем Вот так. Ты кто спросил Он у Вас";
+        String word = "";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Введите слово для того чтобы подсчитать его количество в тексте");
+            word = reader.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        Pattern pattern = Pattern.compile("" + word + "");
+        Matcher matcher = pattern.matcher(text);
+        int counter = 0;
+        while (matcher.find())
+            ++counter;
+        System.out.println(counter);
+    }
+}
