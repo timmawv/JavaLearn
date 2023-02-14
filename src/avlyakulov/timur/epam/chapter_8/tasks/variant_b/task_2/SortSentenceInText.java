55f7c8284f66031eb9e78c4b9f1557d01efaa382
timcol1 <avlyakulovtimur@gmail.com>
2023-02-14 12:05:02 +0200

    finish task with SortSentenceInText

---
+++ src/avlyakulov/timur/epam/chapter_8/tasks/variant_b/task_2/SortSentenceInText.java
@@ -0,0 +1,11 @@
+package avlyakulov.timur.epam.chapter_8.tasks.variant_b.task_2;
+
+import java.util.Arrays;
+
+//Вывести все предложения заданного текста в порядке возрастания количества слов в каждом из них.
+public class SortSentenceInText {
+    public static void main(String[] args) {
+        String text = "Наше первое предложение, было написано вчера вечером. Наше второе предложение, очень хорошее. Наше третье предложение есть";
+        Arrays.stream(text.replaceAll("\\.\\s","\\.").split("\\.")).sorted((s1,s2) -> s1.length() - s2.length()).forEach(System.out::println);
+    }
+}
