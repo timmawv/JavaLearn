6ef15a6be2d4f495f0dd5002e225d62553990da9
timcol1 <avlyakulovtimur@gmail.com>
2023-02-16 09:40:30 +0200

    finish task with DeleteFirstAndLastSymbols

---
+++ src/avlyakulov/timur/epam/chapter_8/tasks/variant_b/task_15/DeleteFirstAndLastSymbols.java
@@ -0,0 +1,23 @@
+package avlyakulov.timur.epam.chapter_8.tasks.variant_b.task_15;
+
+import java.util.Arrays;
+
+/*
+Преобразовать каждое слово в тексте, удалив из него все следующие (предыдущие) вхождения первой (последней) буквы этого слова
+ */
+public class DeleteFirstAndLastSymbols {
+    public static void main(String[] args) {
+        String text = "Наше первое мим шалаш предложение было написано в яру вчера очень поздно вечером." +
+                "Наше второе предложение вчера вечером есть очень хорошее?Наше кабак третье было предложение есть?";
+        String[] words = text.split("[\\s.?]");
+        System.out.println(Arrays.toString(words));
+        text = "";
+        for (int i = 0; i < words.length; ++i) {
+            StringBuilder sb = new StringBuilder(words[i]);
+            if (sb.length() > 2)
+                sb.deleteCharAt(0).deleteCharAt(sb.length() -1);
+            text += sb + " ";
+        }
+        System.out.println(text);
+    }
+}
--- src/avlyakulov/timur/practise/Practise.java
+++ src/avlyakulov/timur/practise/Practise.java
@@ -4,16 +4,10 @@ import java.util.stream.Collectors;
 
 public class Practise {
     public static void main(String[] args) {
-        String sentence = "Hello people It is\n" +
-                "new day of Life";
-        sentence  = sentence.lines().map(String::toLowerCase).collect(Collectors.joining());
-        System.out.println(sentence);
-        String s = "";
-        System.out.println(s.isEmpty());
-        System.out.println(s.isBlank());
-        String str = "азбука";
-        String str1 = "вино";
-        System.out.println(str1.compareToIgnoreCase(str));
+        StringBuilder sb = new StringBuilder("Слово");
+        sb.deleteCharAt(0);
+        sb.deleteCharAt(sb.length() - 1);
+        System.out.println(sb);
 
     }
 }
