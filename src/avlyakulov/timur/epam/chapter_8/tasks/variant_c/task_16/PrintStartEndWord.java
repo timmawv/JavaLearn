1fbe6378b0e21ed83fe5563b5ef49ccad4c11ccc
timcol1 <avlyakulovtimur@gmail.com>
2023-02-20 10:06:12 +0200

    doing tsk with PrintStartEndWord

--- src/avlyakulov/timur/epam/chapter_8/tasks/variant_c/task_16/PrintStartEndWord.java
+++ src/avlyakulov/timur/epam/chapter_8/tasks/variant_c/task_16/PrintStartEndWord.java
@@ -6,13 +6,33 @@ package avlyakulov.timur.epam.chapter_8.tasks.variant_c.task_16;
 порядке, найти такую цепочку, состоящую из наибольшего количества слов.
  */
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class PrintStartEndWord {
     public static void main(String[] args) {
         String text = "армия ярмарка зуй саня одиссей йога";
         String[] words = text.split("\\s");
+        List<String> chain = new ArrayList<>();
+        List<List> chainList = new ArrayList<>();
         System.out.println(Arrays.toString(words));
-
+        int counter = 0;
+        while (counter < words.length) {
+            String word = words[counter];
+            chain.add(word);
+            for (int i = 0; i < words.length; ++i) {
+                for (int j = 0; j < words.length; ++j) {
+                    if (!chain.contains(words[j]) && !word.equals(words[j]) && word.charAt(word.length() - 1) == words[j].charAt(0)) {
+                        word = words[j];
+                        chain.add(word);
+                    }
+                }
+            }
+            chainList.add(chain);
+            ++counter;
+        }
+        for (List l : chainList)
+            System.out.println(l);
     }
 }
