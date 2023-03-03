12dd51ffd34360bae096b7038ebbbb9e8966a402
timcol1 <avlyakulovtimur@gmail.com>
2023-03-03 13:46:58 +0200

    add task with FindSpecialTypeInFile

---
+++ src/avlyakulov/timur/epam/chapter_10/variant_c/task_6/FindSpecialTypeInFile.java
@@ -0,0 +1,10 @@
+package avlyakulov.timur.epam.chapter_10.variant_c.task_6;
+/*
+Файл содержит символы, слова, целые числа и числа с плавающей запятой.
+Определить все данные, тип которых вводится из командной строки.
+ */
+public class FindSpecialTypeInFile {
+    public static void main(String[] args) {
+
+    }
+}
