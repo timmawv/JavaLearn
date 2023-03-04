c1e7e7f965c4a02a203e63f3298c35feaf6ab8e5
timcol1 <avlyakulovtimur@gmail.com>
2023-03-04 09:03:15 +0200

    finish task with leetcode

--- src/avlyakulov/timur/practise/leetcode/PractiseTask.java
+++ src/avlyakulov/timur/practise/leetcode/PractiseTask.java
@@ -4,27 +4,41 @@ import java.util.Arrays;
 
 public class PractiseTask {
 
-    //todo доделать эту задачу с литкода
     public int[] shuffle(int[] nums, int n) {
-        int beginCounter = 1;
-        for (int i = n; i < nums.length; ++i) {
-            int c = nums[beginCounter];
-            nums[beginCounter] = nums[i];
-            nums[i] = c;
-            ++beginCounter;
-            System.out.println(Arrays.toString(nums));
+        //1 способ
+        int counter = 1;
+        for (int j = n; j < nums.length - 1; ++j) {
+            for (int i = j; i > counter; --i) {
+                int c = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = c;
+            }
+            ++counter;
+            ++counter;
         }
         return nums;
     }
 
+    public int[] shuffleFaster(int[] nums, int n) {
+        //2 способ
+        int[] arr = new int[2 * n];
+        for (int i = 0; i < n; ++i) {
+            arr[2 * i] = nums[i];
+            arr[2 * i + 1] = nums[n + i];
+        }
+        return arr;
+    }
+
+
     public static void main(String[] args) {
         int[] nums = {2, 5, 1, 3, 4, 7};
         System.out.println("Base : [2, 5, 1, 3, 4, 7]");
-        nums = new PractiseTask().shuffle(nums, 3);
+        nums = new PractiseTask().shuffleFaster(nums, 3);
         System.out.println("Result : " + Arrays.toString(nums));
         System.out.println("Need : [2, 3, 5, 4, 1, 7]");
         // x1 x2 x3 y1 y2 y3 - начало
-        // x1 y1 x3 x2 y2 y3
+        // x1 x2 y1 x3 y2 y3 - 1 перестановка
+        // x1 y1 x2 x3 y2 y3 - 2 перестановка (n - 1 шаг)
 
 
         // x1 x2 x3 x4 x5 y1 y2 y3 y4 y5 - начало
