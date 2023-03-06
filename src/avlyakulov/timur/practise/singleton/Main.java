da0928e3f2a70d2d8c9e378a474cb5b0946e34f2
timcol1 <avlyakulovtimur@gmail.com>
2023-03-06 17:42:35 +0200

    add simple singletone and explicit constructor invocation

---
+++ src/avlyakulov/timur/practise/singleton/Main.java
@@ -0,0 +1,11 @@
+package avlyakulov.timur.practise.singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BankAccount account = BankAccount.getInstance("John",120);
+        BankAccount account1 = BankAccount.getInstance("Diego",200);
+        System.out.println(account);
+        System.out.println(account1);
+    }
+}
