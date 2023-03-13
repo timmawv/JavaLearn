6bd94eb0ee3e610a65059877be6c7746a079faa2
timcol1 <avlyakulovtimur@gmail.com>
2023-03-13 22:07:01 +0200

    finish task with RestoreString

--- src/avlyakulov/timur/practise/leetcode/RestoreString.java
+++ src/avlyakulov/timur/practise/leetcode/RestoreString.java
@@ -2,14 +2,12 @@ package avlyakulov.timur.practise.leetcode;
 
 public class RestoreString {
     public String restoreString(String s, int[] indices) {
-        StringBuilder result = new StringBuilder(s.length());
-        for (int i : indices) {
-            System.out.println(s.charAt(i));
-            result.setCharAt(i,s.charAt(i));
+        char[] word = new char[indices.length];
+        for (int i = 0; i < indices.length; ++i) {
+            word[indices[i]] = s.charAt(i);
         }
-        return result.toString();
+        return String.valueOf(word);
     }
-
     public static void main(String[] args) {
         String s = "codeleet";
         int[] indicies = {4, 5, 6, 7, 0, 2, 1, 3};
