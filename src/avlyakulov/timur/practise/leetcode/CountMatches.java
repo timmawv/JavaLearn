fc4929e8323ed69c6a257ec59ce0375092089de0
timcol1 <avlyakulovtimur@gmail.com>
2023-03-17 10:11:29 +0200

    add some finished tasks

---
+++ src/avlyakulov/timur/practise/leetcode/CountMatches.java
@@ -0,0 +1,20 @@
+package avlyakulov.timur.practise.leetcode;
+
+import java.util.List;
+
+public class CountMatches {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int typeNum = 0;
+        int counter = 0;
+        switch (ruleKey) {
+            case "color" -> typeNum = 1;
+            case "name" -> typeNum = 2;
+        }
+        for (List<String> l : items) {
+            if (l.get(typeNum).equals(ruleValue))
+                ++counter;
+        }
+        return counter;
+    }
+
+}
--- src/avlyakulov/timur/practise/leetcode/Test.java
+++ src/avlyakulov/timur/practise/leetcode/Test.java
@@ -2,8 +2,15 @@ package avlyakulov.timur.practise.leetcode;
 
 
 public class Test {
-
+    public int xorOperation(int n, int start) {
+        int sum = start;
+        for (int i = 0; i < n - 1; ++i) {
+            sum = sum ^ (start + 2);
+            start += 2;
+        }
+        return sum;
+    }
     public static void main(String[] args) {
-
+        System.out.println(new Test().xorOperation(5,0));
     }
 }
