545d43b6712b329baa43aebf7e3e619b86b2191e
timcol1 <avlyakulovtimur@gmail.com>
2023-03-22 18:07:41 +0200

    add task with DecodeMessage

--- src/avlyakulov/timur/practise/leetcode/Test.java
+++ src/avlyakulov/timur/practise/leetcode/Test.java
@@ -4,17 +4,21 @@ package avlyakulov.timur.practise.leetcode;
 import avlyakulov.timur.algorithms.Tree;
 
 public class Test {
-    public int xorOperation(int n, int start) {
-        int sum = start;
-        for (int i = 0; i < n - 1; ++i) {
-            sum = sum ^ (start + 2);
-            start += 2;
+    public String sortSentence(String s) {
+        String[] words = s.split("\\s");
+        String result = "";
+        for (int i = 1; i < words.length + 1; ++i) {
+            for (String w : words)
+                if (Character.getNumericValue(w.charAt(w.length() - 1)) == i) {
+                    result = i == 1 ? result.concat(w.substring(0,w.length() - 1)) : result.concat(" " + w.substring(0,w.length() - 1));
+                }
         }
-        return sum;
+        return result;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(new Test().xorOperation(5,0));
+        System.out.println(new Test().sortSentence("is2 sentence4 This1 a3"));
+
     }
 }
