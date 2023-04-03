dc060a3fc7cd94a581538332f4f04203167eb23b
timcol1 <avlyakulovtimur@gmail.com>
2023-04-03 12:04:17 +0300

    add copy of collections

--- src/avlyakulov/timur/practise/Practise.java
+++ src/avlyakulov/timur/practise/Practise.java
@@ -1,24 +1,20 @@
 package avlyakulov.timur.practise;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Practise {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-    }
-}
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(15);
+        list.add(16);
+        List<Integer> list1 = new ArrayList<>(list);
+        list1.remove(1);
+        System.out.println(list);
+        System.out.println(list1);
 
-class Car implements Comparable<Car> {
-    int cost;
-    String name;
-
-    public Car(int cost, String name) {
-        this.cost = cost;
-        this.name = name;
-    }
-
-    @Override
-    public int compareTo(Car o) {
-        return this.cost - o.cost;
     }
 }
