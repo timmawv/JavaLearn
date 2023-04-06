f2bd081cf68cae4e0e138d2d363ce890948bb9e2
timcol1 <avlyakulovtimur@gmail.com>
2023-04-06 12:05:58 +0300

    add logic of task

--- src/avlyakulov/timur/practise/leetcode/LargestLocalValueInMatrix.java
+++ src/avlyakulov/timur/practise/leetcode/LargestLocalValueInMatrix.java
@@ -3,8 +3,16 @@ package avlyakulov.timur.practise.leetcode;
 import avlyakulov.timur.epam.chapter_2.matrix.OutputMatrix;
 
 public class LargestLocalValueInMatrix {
+    //   3 3 3 9
+    //   1 2 5 7
+    //   1 1 4 6
+    //   1 1 4 5
+    //   answer:
+    //   5 9
+    //   5 7
     public static int[][] largestLocal(int[][] grid) {
         int[][] matrix = new int[grid.length - 2][grid.length - 2];
+        int length = matrix.length;
 
         return matrix;
     }
