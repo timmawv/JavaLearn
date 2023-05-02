43d32d225fce6dd1967b45b338ae33580c174d2d
timcol1 <avlyakulovtimur@gmail.com>
2023-05-02 17:01:32 +0300

    add task with FindFirstMaxAndSecond

--- src/avlyakulov/timur/practise/Practise.java
+++ src/avlyakulov/timur/practise/Practise.java
@@ -2,8 +2,20 @@ package avlyakulov.timur.practise;
 
 
 public class Practise {
+    public int subsetXORSum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            sum += nums[i];
+            for (int x = i + 1; x < nums.length; ++x) {
+                sum += nums[i] ^ nums[x];
+            }
+        }
+
+        return sum;
+    }
 
     public static void main(String[] args) {
-
+        int[] arr = {1,3};
+        System.out.println(new Practise().subsetXORSum(arr));
     }
 }
