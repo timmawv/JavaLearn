dc7bfc4ad21dcee79331c48386803c32e5238fda
timcol1 <avlyakulovtimur@gmail.com>
2023-05-12 00:13:44 +0300

    add task with DescryptAlphabet

---
+++ src/avlyakulov/timur/practise/leetcode/DescryptAlphabet.java
@@ -0,0 +1,14 @@
+package avlyakulov.timur.practise.leetcode;
+
+public class DescryptAlphabet {
+    public String freqAlphabets(String s) {
+
+    }
+
+    public static void main(String[] args) {
+        /*String s = "10#11#12";
+        System.out.println(new DescryptAlphabet().freqAlphabets(s));*/
+        //char num = 97;
+
+    }
+}
