7e0919ad8d9fbc8c350d5b8c0ea35e36f45dfbb6
timcol1 <avlyakulovtimur@gmail.com>
2023-05-18 17:15:30 +0300

    add realization to count to last #

--- src/main/java/avlyakulov/timur/practise/leetcode/DescryptAlphabet.java
+++ src/main/java/avlyakulov/timur/practise/leetcode/DescryptAlphabet.java
@@ -1,29 +1,38 @@
 package avlyakulov.timur.practise.leetcode;
 
+import java.io.CharArrayReader;
 import java.util.Arrays;
 
 public class DescryptAlphabet {
     public String freqAlphabets(String s) {
         String result = "";
-        String[] words = s.split("#");
-        System.out.println(Arrays.toString(words));
-        char num = 96;
-        for (int j = 0; j < words.length; ++j)
-            for (int i = 0; i < words[j].length(); ++i) {
-                System.out.println(words[j].charAt(i));
-                char temp = (char) (words[j].charAt(i) + num);
-                System.out.println(temp);
-                result = result.concat(String.valueOf(temp));
+        int num = 96;
+        String[] word = s.split("#");
+        for (int i = 0; i < word.length; ++i) {
+            if (i == word.length - 1) {
+                for (int x = 0; x < word[i].length(); ++x) {
+                    char curNum = word[i].charAt(x);
+                    num = num + curNum;
+                    result = result.concat(Character.toString(num));
+                }
+            } else {
+                int curNum = Integer.parseInt(word[i]);
+                result = result.concat(Character.toString(num + curNum));
             }
+        }
 
         return result;
     }
 
     public static void main(String[] args) {
-        String s = "1326#";
+        String s = "10#11#12";
         System.out.println(new DescryptAlphabet().freqAlphabets(s));
         char num = 96;
-        System.out.println((char) (1 + num));
+        char n = 1;
+        String ab = Character.toString(num + n);
+        System.out.println(ab);
+        String a = Character.toString(num + 1);
+        System.out.println(a);
 
     }
 }
