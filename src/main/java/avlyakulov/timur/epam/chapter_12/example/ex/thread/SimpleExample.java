d02a6a610b07ceb9dd03ecaf4d80f4595ab3ceed
timcol1 <avlyakulovtimur@gmail.com>
2023-05-30 09:17:12 +0300

    add example of simple threads

---
+++ src/main/java/avlyakulov/timur/epam/chapter_12/example/ex/thread/SimpleExample.java
@@ -0,0 +1,28 @@
+package avlyakulov.timur.epam.chapter_12.example.ex.thread;
+
+public class SimpleExample {
+    public static void main(String[] args) {
+        System.out.println("Example of main thread " + Thread.currentThread().getName());
+        MyThread myThread = new MyThread();
+        myThread.start();
+        MyRunnable myRunnable = new MyRunnable();
+        //myRunnable.run();// выводится main потом не создался
+        Thread thread = new Thread(myRunnable);// теперь поток создается и будет корректно работать
+        thread.start();
+    }
+
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("It is extend " + Thread.currentThread().getName());
+    }
+}
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("It is runnable " + Thread.currentThread().getName());
+    }
+}
