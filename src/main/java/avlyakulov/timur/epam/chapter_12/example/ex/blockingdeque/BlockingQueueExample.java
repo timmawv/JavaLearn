a7d6f46362bc06153d8280c0d8c6a01980694e96
timcol1 <avlyakulovtimur@gmail.com>
2023-06-13 10:47:09 +0300

    add example with BlockingQueue

---
+++ src/main/java/avlyakulov/timur/epam/chapter_12/example/ex/blockingdeque/BlockingQueueExample.java
@@ -0,0 +1,24 @@
+package avlyakulov.timur.epam.chapter_12.example.ex.blockingdeque;
+
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class BlockingQueueExample {
+    //BlockingQueue - решает проблему, когда есть очередь и может быть такое что поток берет эелемент которого еще нет.
+    public static void main(String[] args) {
+        BlockingQueue<String> queue = new PriorityBlockingQueue<>();
+
+        new Thread(() -> {
+            try {
+                System.out.println(queue.take());//то же самое что и remove, только он блокируется в моменте, не будет выполняться, пока туда не положим элемент, чтоб его достать
+                //скорей всего работает s notify wait.
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+        new Thread(() -> queue.add("this is String")).start();
+    }
+}
