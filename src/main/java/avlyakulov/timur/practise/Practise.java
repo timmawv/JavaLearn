487e53869876932c4cf7722b237ef03a9f4efc36
timcol1 <avlyakulovtimur@gmail.com>
2023-06-14 13:16:24 +0300

    add task with chessboard

--- src/main/java/avlyakulov/timur/practise/Practise.java
+++ src/main/java/avlyakulov/timur/practise/Practise.java
@@ -1,32 +1,22 @@
 package avlyakulov.timur.practise;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Practise {
 
-    public List<Integer> selfDividingNumbers(int left, int right) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = left; i <= right; ++i) {
-            int length = String.valueOf(i).length();
-            int counter = 0;
-            int temp = i;
-            for (int j = 0; j < length; ++j) {
-                int n = temp % 10;
-                if (n == 0)
-                    break;
-                if (i % n == 0)
-                    ++counter;
-                temp /= 10;
-            }
-            if (counter == length)
-                list.add(i);
+    public boolean squareIsWhite(String coordinates) {
+        char n = coordinates.charAt(0);
+        int num = n - 96;
+        int pos = coordinates.charAt(1) - 48;
+        if (num % 2 == 0) {
+            return pos % 2 != 0;
+        } else {
+            return pos % 2 == 0;
         }
-        return list;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Practise().selfDividingNumbers(688, 690));
+        String coordinates = "c1";
+        int pos = coordinates.charAt(1) - 48;
+        System.out.println(pos);
     }
 }
