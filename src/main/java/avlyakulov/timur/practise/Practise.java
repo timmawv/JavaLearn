a84fcdcaa4d529dbb12eb12f585bbe7bf13d89ed
timcol1 <avlyakulovtimur@gmail.com>
2023-06-19 23:33:49 +0300

    add task with distringMatch

--- src/main/java/avlyakulov/timur/practise/Practise.java
+++ src/main/java/avlyakulov/timur/practise/Practise.java
@@ -1,19 +1,26 @@
 package avlyakulov.timur.practise;
 
 
+import java.util.Arrays;
+
 public class Practise {
     public int[] diStringMatch(String s) {
         int[] arr = new int[s.length() + 1];
-        arr[0] = (int) (Math.random() * 3 + 3);
-        int counter = 1;
-        for (char c : s.toCharArray()) {
-            if (c == 'I') {
-
+        int counterI = 0;
+        int counterD = s.length();
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == 'I') {
+                arr[i] = counterI++;
+            } else {
+                arr[i] = counterD--;
             }
         }
+        arr[s.length()] = s.charAt(s.length() - 1) == 'I' ? counterI : counterD;
+        return arr;
     }
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Practise().diStringMatch("IDID")));
 
     }
 }
