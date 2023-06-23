d05df558740d7396ee063a9ac090352ff635797a
timcol1 <avlyakulovtimur@gmail.com>
2023-06-23 16:52:18 +0300

    add task with reverse number

--- src/main/java/avlyakulov/timur/practise/Practise.java
+++ src/main/java/avlyakulov/timur/practise/Practise.java
@@ -1,25 +1,17 @@
 package avlyakulov.timur.practise;
 
 
-import java.util.Arrays;
-
 public class Practise {
-    public int[] diStringMatch(String s) {
-        int[] arr = new int[s.length() + 1];
-        int counterI = 0;
-        int counterD = s.length();
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) == 'I') {
-                arr[i] = counterI++;
-            } else {
-                arr[i] = counterD--;
-            }
-        }
-        arr[s.length()] = s.charAt(s.length() - 1) == 'I' ? counterI : counterD;
-        return arr;
+
+    public boolean isSameAfterReversals(int num) {
+        StringBuilder str = new StringBuilder(String.valueOf(num));
+        String numRes = String.valueOf(str.reverse());
+        String strReverse = String.valueOf(new StringBuilder(String.valueOf(Integer.parseInt(String.valueOf(str)))));
+        return numRes.equals(strReverse);
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Practise().diStringMatch("IDID")));
+        int num = 625;
+        System.out.println(new Practise().isSameAfterReversals(num));
     }
 }
