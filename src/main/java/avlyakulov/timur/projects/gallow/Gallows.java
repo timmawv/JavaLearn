12cccaf027652afb251368be425b862ab2df2d48
timcol1 <avlyakulovtimur@gmail.com>
2023-06-25 16:49:21 +0300

    add state of gallows

--- src/main/java/avlyakulov/timur/projects/gallow/Gallows.java
+++ src/main/java/avlyakulov/timur/projects/gallow/Gallows.java
@@ -2,4 +2,80 @@ package avlyakulov.timur.projects.gallow;
 
 public class Gallows {
     public int life;
+
+    public Gallows() {
+        this.life = 5;
+    }
+
+    public void gallowsState() {
+        switch (life) {
+            case 5 -> {
+                System.out.println("\t----|");
+                System.out.println("\t|\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("\t-------");
+            }
+            case 4 -> {
+                System.out.println("\t----|");
+                System.out.println("\t|\t|");
+                System.out.println("  \tO\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("\t-------");
+            }
+            case 3 -> {
+                System.out.println("\t----|");
+                System.out.println("\t|\t|");
+                System.out.println("  \tO\t|");
+                System.out.println("  \t|\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("\t-------");
+            }
+            case 2 -> {
+                System.out.println("\t----|");
+                System.out.println("\t|\t|");
+                System.out.println("  \tO\t|");
+                System.out.println("   _|_\t|");
+                System.out.println("\t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("\t-------");
+            }
+            case 1 -> {
+                System.out.println("\t----|");
+                System.out.println("\t|\t|");
+                System.out.println("   _O_\t|");
+                System.out.println("   \t|\t|");
+                System.out.println("   / \t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("\t-------");
+            }
+            case 0 -> {
+                System.out.println("\t----|");
+                System.out.println("\t|\t|");
+                System.out.println("   _O_\t|");
+                System.out.println("   \t|\t|");
+                System.out.println("   / \\\t|");
+                System.out.println("  \t\t|");
+                System.out.println("  \t\t|");
+                System.out.println("\t-------");
+            }
+        }
+
+    }
+
+    public void startGame() {
+        System.out.println("The game has started ");
+        gallowsState();
+
+    }
 }
--- src/main/java/avlyakulov/timur/projects/gallow/GallowsMain.java
+++ src/main/java/avlyakulov/timur/projects/gallow/GallowsMain.java
@@ -2,13 +2,6 @@ package avlyakulov.timur.projects.gallow;
 
 public class GallowsMain {
     public static void main(String[] args) {
-        System.out.println("\t----|");
-        System.out.println("\t|\t|");
-        System.out.println("  \t\t|");
-        System.out.println("  \t\t|");
-        System.out.println("  \t\t|");
-        System.out.println("  \t\t|");
-        System.out.println("  \t\t|");
-        System.out.println("\t-------");
+        new Gallows().startGame();
     }
 }
