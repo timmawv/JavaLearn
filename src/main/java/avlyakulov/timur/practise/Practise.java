4635759055f7c38d5716331a7ddd75cebe71b2b5
timcol1 <avlyakulovtimur@gmail.com>
2023-07-03 13:24:45 +0300

    add task with prefix count

--- src/main/java/avlyakulov/timur/practise/Practise.java
+++ src/main/java/avlyakulov/timur/practise/Practise.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
 
 public class Practise {
 
-    public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int sum = 0;
-        for (int i = 0; i < nums.length; i += 2) {
-            sum += nums[i] < nums[i + 1] ? nums[i] : nums[i + 1];
-        }
-        return sum;
+    public int prefixCount(String[] words, String pref) {
+        int counter = 0;
+        for (String w : words)
+            if (w.startsWith(pref))
+                ++counter;
+        return counter;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 4, 3, 2};
-        System.out.println(new Practise().arrayPairSum(nums));
+        String[] words = {"pay", "attention", "practice", "attend"};
+        String pref = "at";
+        System.out.println(new Practise().prefixCount(words, pref));
     }
 }
