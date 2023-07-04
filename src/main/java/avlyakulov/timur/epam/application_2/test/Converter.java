5c76e3642f95eac433dbd6ac38231cf91f03a966
timcol1 <avlyakulovtimur@gmail.com>
2023-07-04 10:54:04 +0300

    add test for class Converter of temperature

---
+++ src/main/java/avlyakulov/timur/epam/application_2/test/Converter.java
@@ -0,0 +1,23 @@
+package avlyakulov.timur.epam.application_2.test;
+
+public class Converter {
+    private static final int ABSOLUTE_ZERO = -273;
+
+
+    public double convertCelsiusToFahrenheit(double celsius) {
+        if (celsius < ABSOLUTE_ZERO) {
+            throw new IllegalArgumentException("Error data");
+        }
+        double fahrenheit = celsius * 1.8 + 32;
+        return fahrenheit;
+    }
+
+    public boolean checkCelsius(double celsius) {
+        return celsius >= ABSOLUTE_ZERO;
+    }
+
+    public static void main(String[] args) {
+        Converter converter = new Converter();
+        System.out.println(converter.checkCelsius(-300));
+    }
+}
---
+++ src/test/java/ConverterTest.java
@@ -0,0 +1,30 @@
+import avlyakulov.timur.epam.application_2.test.Converter;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class ConverterTest {
+    Converter converter = new Converter();
+
+    @Test()
+    public void testConvertCelsiusToFahrenheit() {
+        double actual = converter.convertCelsiusToFahrenheit(10.0);
+        double expected = 50.;
+        assertEquals(actual, expected, 0.001, " Test failed as...");
+    }
+
+    @Test
+    public void checkSameTest() {
+        String actual = "java" + 17;
+        String expected = "ja" + "va" + 17;
+        Assert.assertSame(actual, expected);
+    }
+    @Test
+    public void testCheckCelsiusFalse() {
+        Converter converter1 = new Converter();
+        boolean condition = converter1.checkCelsius(-300);
+        Assert.assertFalse(condition, "test false failed as...");
+    }
+}
