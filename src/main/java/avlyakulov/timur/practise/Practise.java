78060385969d07eebebf8bed3f6189b7771d0bcb
timcol1 <avlyakulovtimur@gmail.com>
2023-07-12 13:16:09 +0300

    add task with Check if All Characters Have Equal Number of Occurrences

--- src/main/java/avlyakulov/timur/practise/Practise.java
+++ src/main/java/avlyakulov/timur/practise/Practise.java
@@ -1,24 +1,67 @@
 package avlyakulov.timur.practise;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Practise {
 
-    public int findNumbers(int[] nums) {
-        int counter = 0;
-        for (int num : nums) {
-            if (digits(num) % 2 == 0)
-                ++counter;
+    /*public boolean areOccurrencesEqual(String s) {
+        boolean checkGoodString = true;
+        char[] arr = s.toCharArray();
+        int mainCounter = 0;
+        for (int i = 1; i < arr.length; ++i) {
+            if (arr[0] == arr[i]) {
+                ++mainCounter;
+            }
         }
-        return counter;
-    }
+        for (int i = 0; i < arr.length; ++i) {
+            int counter = 0;
+            for (int j = 0; j < arr.length;++j) {
+                if (i!=j && arr[i] != arr[0]) {
+                    if (arr[i] == arr[j])
+                        ++counter;
+                }
+            }
+            if (counter != mainCounter) {
+                checkGoodString = false;
+                break;
+            }
 
+        }
+        return checkGoodString;
+    }*/
+    public boolean areOccurrencesEqual(String s) {
+        boolean checkGoodString = true;
+        Set<Character> set = new TreeSet<>();
+        char[] arr = s.toCharArray();
+        for (char c : arr)
+            set.add(c);
+        int[] hitLetters = new int[set.size()];
+        int posArr = 0;
+        for (char c : set) {
+            int counter = 0;
+            for (int i = 0; i < arr.length; ++i) {
+                if (c == arr[i])
+                    ++counter;
+            }
+            hitLetters[posArr] = counter;
+            ++posArr;
+        }
+        for (int i = 0; i < hitLetters.length; ++i) {
+            for (int j = i + 1; j < hitLetters.length; ++j) {
+                if (hitLetters[i] != hitLetters[j]) {
+                    checkGoodString = false;
+                    break;
+                }
+            }
+        }
 
-    public static int digits(int n) {
-        if (n < 10) return 1;
-        return 1 + digits(n / 10);
+        return checkGoodString;
     }
 
     public static void main(String[] args) {
-        int[] nums = {12, 345, 2, 6, 7896};
-        System.out.println(new Practise().findNumbers(nums));
+        String s = "abacbc";
+        System.out.println(new Practise().areOccurrencesEqual(s));
     }
 }
