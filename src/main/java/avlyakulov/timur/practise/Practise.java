5aae3b6415acf7b12393e575abc653eb39df4c86
timcol1 <avlyakulovtimur@gmail.com>
2023-10-16 11:32:13 +0300

    add example with inheritance and polymorphism

--- src/main/java/avlyakulov/timur/practise/Practise.java
+++ src/main/java/avlyakulov/timur/practise/Practise.java
@@ -2,13 +2,27 @@ package avlyakulov.timur.practise;
 
 public class Practise {
 
+    public void test() {
+        System.out.println("It is test");
+    }
+
+    public void test(int a) {
+        System.out.println("It is test with int " + a);
+    }
+
+    public void test(double a) {
+        System.out.println("It is test with double " + a);
+    }
+
 
     public static void main(String[] args) {
-        String test = "simple test";
-        StringBuilder stringBuilder = new StringBuilder("simple test");
-        test.toLowerCase();
-        stringBuilder.reverse();
-        System.out.println(test);//значение не изменилось никак
-        System.out.println(stringBuilder);//значение изменилось та как stringBuilder mutable object
+        Practise practise = new NewPractise();
+        practise.test();
+    }
+}
+
+class NewPractise extends Practise {
+    public void test() {
+        System.out.println("It is a new test");
     }
 }
