58e2053bf6ff056ff410e6a1ec87918f8852265a
timcol1 <avlyakulovtimur@gmail.com>
2023-10-16 10:48:38 +0300

    add example for mutable and immutable object

--- src/main/java/avlyakulov/timur/practise/Practise.java
+++ src/main/java/avlyakulov/timur/practise/Practise.java
@@ -1,30 +1,14 @@
 package avlyakulov.timur.practise;
 
 public class Practise {
-    static class InterviewBit {
-        InterviewBit() {
-            System.out.println(" Welcome to InterviewBit ");
-        }
-    }
 
-    static class ScalerAcademy extends InterviewBit {
-        ScalerAcademy() {
-            super();
-            System.out.println(" Welcome to Scaler Academy by InterviewBit");
-        }
-
-        int ScalerAcademy(int a) {
-            return 2;
-        }
-    }
-
-    static class Example {
-        Example() {
-            System.out.println("Hell gyus");
-        }
-    }
 
     public static void main(String[] args) {
-        ScalerAcademy sc = new ScalerAcademy();
+        String test = "simple test";
+        StringBuilder stringBuilder = new StringBuilder("simple test");
+        test.toLowerCase();
+        stringBuilder.reverse();
+        System.out.println(test);//значение не изменилось никак
+        System.out.println(stringBuilder);//значение изменилось та как stringBuilder mutable object
     }
 }
