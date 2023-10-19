7c4947b50a3585e66eeda519c92d8237de88547a
timcol1 <avlyakulovtimur@gmail.com>
2023-10-19 10:27:34 +0300

    add good example with regex java

--- src/main/java/avlyakulov/timur/practise/RegexJavaPractise.java
+++ src/main/java/avlyakulov/timur/practise/RegexJavaPractise.java
@@ -2,8 +2,7 @@ package avlyakulov.timur.practise;
 
 import java.util.regex.Pattern;
 
-public class Practise {
-
+public class RegexJavaPractise {
     public static void main(String[] args) {
         String text = "Hel2o peo7ple it is ou6r wo3rld gu4ys";
         text = Pattern.compile("([a-z])(\\d)")
