3144f8d6376b242b631625c3ab4a0d7d207ebed4
Timur <91984444+timcol1@users.noreply.github.com>
2024-03-06 11:28:34 +0200

    remove hibernate package

--- src/main/java/avlyakulov/timur/self_practise/other/hibernate/model/Phone.java
+++
@@ -1,7 +0,0 @@
-package avlyakulov.timur.self_practise.other.hibernate.model;
-
-public enum Phone {
-    IPHONE,
-    SAMSUNG,
-    PIXEL
-}
--- src/main/java/avlyakulov/timur/self_practise/other/hibernate/util/CapitalizeFirstLetter.java
+++
@@ -1,7 +0,0 @@
-package avlyakulov.timur.self_practise.other.hibernate.util;
-
-public class CapitalizeFirstLetter {
-    public static String capitalizeFirstLetter(String input) {
-        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
-    }
-}
