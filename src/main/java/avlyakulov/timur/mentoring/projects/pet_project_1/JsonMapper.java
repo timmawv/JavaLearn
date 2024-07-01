281eac19813aa80b1debdbf2bbe5da89bb66f5ab
Timur <91984444+timcol1@users.noreply.github.com>
2024-07-01 12:26:30 +0300

    finish with json mapper

--- src/main/java/avlyakulov/timur/mentoring/projects/pet_project_1/JsonMapper.java
+++ src/main/java/avlyakulov/timur/mentoring/projects/pet_project_1/JsonMapper.java
@@ -6,46 +6,55 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JsonMapper {
 
-    public static <T> T readFromString(String raw, Class<T> clazz) {
-        String rawString = raw.replace("{", "").replace("}", "").trim();
-        String[] split = rawString.split("\n");
-        Map<String, String> map = new HashMap<>();
+    public static <T> T readFromString(String json, Class<T> clazz) {
+        String[] jsonLines = splitJsonForLines(json);
+        Map<String, String> jsonKeyValue = fillMapWithJsonValues(jsonLines);
+        return setFieldsToObject(jsonKeyValue, clazz);
+    }
 
-        for (String str : split) {
+    public static String[] splitJsonForLines(String json) {
+        String rawString = json.replace("{", "").replace("}", "").trim();
+        return rawString.split("\n");
+    }
+
+    public static Map<String, String> fillMapWithJsonValues(String[] lines) {
+        Map<String, String> map = new HashMap<>();
+        for (String str : lines) {
             str = str.replaceAll("[\",]", "").trim();
-            String[] split1 = str.split(":");
-            if (split1.length == 2) {
-                map.put(split1[0].trim(), split1[1].trim());
+            String[] entry = str.split(":");
+            if (entry.length == 2) {
+                map.put(entry[0].trim(), entry[1].trim());
             }
         }
+        return map;
+    }
+
+    public static <T> T setFieldsToObject(Map<String, String> jsonKeyValue, Class<T> clazz) {
         T obj;
         try {
             obj = clazz.getDeclaredConstructor().newInstance();
-        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
-                 InvocationTargetException e) {
+        } catch (Exception e) {
             throw new RuntimeException("Error with creating object");
         }
         Field[] fields = obj.getClass().getDeclaredFields();
         for (Field field : fields) {
             field.setAccessible(true);
-
             try {
                 if (field.getType() == int.class || field.getType() == Integer.class) {
-                    String value = map.get(field.getName());
+                    String value = jsonKeyValue.get(field.getName());
                     field.setInt(obj, Integer.parseInt(value));
                 }
                 if (field.getType() == boolean.class || field.getType() == Boolean.class) {
-                    String value = map.get(field.getName());
+                    String value = jsonKeyValue.get(field.getName());
                     field.setBoolean(obj, Boolean.parseBoolean(value));
                 }
                 if (field.getType() == String.class) {
-                    String value = map.get(field.getName());
+                    String value = jsonKeyValue.get(field.getName());
                     field.set(obj, value);
                 }
             } catch (IllegalAccessException e) {
@@ -55,7 +64,7 @@ public class JsonMapper {
         return obj;
     }
 
-    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
+    public static void main(String[] args) {
         String rawString = """
                 {
                   "name": "Timur",
